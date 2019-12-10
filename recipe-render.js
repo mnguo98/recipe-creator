b2d155507db2588e92d47e3cc11d0942b6ffc573
Brendan Mohr <bmohr98@live.unc.edu>
2019-12-10 15:55:34 -0500

    renders recipes

---
+++ recipe-render.js
@@ -0,0 +1,38 @@
+
+//82278202631f441d86817f98bb2f9f49
+//5a38fb37355b47168d33551d70f5b0ec
+export async function loadRec() {
+    const $root = $('#root');
+    const result = await axios({
+        method: 'get',
+        url: 'https://api.spoonacular.com/recipes/random?apiKey=5a38fb37355b47168d33551d70f5b0ec',
+        number:1
+      });
+      let rec = result.data;
+      console.log(rec.recipes[0]);
+      let inglist = [];
+      for(let i=0;i<rec.recipes[0].extendedIngredients.length;i++)
+      {
+        var ing = rec.recipes[0].extendedIngredients[i].original;
+        var editing = ing.replace(/,/g,'')
+        inglist.push('<p>'+editing+'</p>');
+      }
+      $root.append(
+    '<div>'
+    +'<h1>' + rec.recipes[0].title + '</h1>'
+    +'<img src='+rec.recipes[0].image + 'alt="Recipe Image" ></img>'
+    +'<h2> Cook Time: '+rec.recipes[0].readyInMinutes+' Minutes </h2>'
+    +'<h2> Number of Servings: '+rec.recipes[0].servings+' Portions </h2>'
+    +'<h2> Part of these Diet(s): '+rec.recipes[0].diets+' </h2>'
+    +'<h2> Ingredients: </h2>'
+    +'<ul>'
+    +inglist
+    +'</ul>'
+    +'<h2> Instructions: </h2>'
+    +'<h3>'+rec.recipes[0].instructions+'</h3>'
+    +'</div>');
+}
+
+$(function() {
+    loadRec();
+});
